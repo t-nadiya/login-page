983c7b7507387341531614797aa469e383379167
Nadiia Tytarenko <nadiya.tita@gmail.com>
2022-08-17 15:10:46 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login-page', loadChildren: () => import('./components/login-page/login-page.module').then(m => m.LoginPageModule) },
+  { path: 'main-page', loadChildren: () => import('./components/main-page/main-page.module').then(m => m.MainPageModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
